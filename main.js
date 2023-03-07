4c985d8b894e13ec28584bff116d22fdd14a9165
Kalyan-1412 <904018@ssis.asia>
2023-03-07 19:54:16 +0800

    Changes 2

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
 import './style.css';
+
+const img = document.getElementById('img');console.log(img);getRandomPerson();async function getRandomPerson() {  const data = await fetch('https://randomuser.me/api/');  const person = await data.json();  img.src = person.results[0].picture.large;}
