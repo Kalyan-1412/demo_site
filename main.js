c21d8764eb234ce49ccbe69cbd5f2899b18809fc
Kalyan-1412 <904018@ssis.asia>
2023-03-09 15:15:58 +0800

    3

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 import './style.css';
 
-const img = document.getElementById('img');console.log(img);getRandomPerson();async function getRandomPerson() {  const data = await fetch('https://randomuser.me/api/');  const person = await data.json();  img.src = person.results[0].picture.large;}
+//'https://randomuser.me/api/'
+
+const img = document.getElementById('img');
+console.log(img);
+
+getRandomPerson();
+async function getRandomPerson() {
+  const data = await fetch('https://randomuser.me/api/');
+  const person = await data.json();
+  img.src = person.results[0].picture.large;
+}
+
+
